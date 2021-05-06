7e6a82615cae30ebfde93f1ca6e9cfaa0bc1d16d
Jesse <jessemckinney513@gmail.com>
2021-05-05 22:42:14 -0400

    yay

---
+++ src/Components/Button/Button.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styling from "./Button.module.css";
+
+function Button(props) {
+  return (
+    <>
+      <div className={styling.wrapping}></div>
+    </>
+  );
+}
+
+export default Button;
