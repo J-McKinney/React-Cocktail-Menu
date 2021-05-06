473ae4f9094f534da82fae827565494ed8c0c03c
Jesse <jessemckinney513@gmail.com>
2021-05-06 13:31:59 -0400

    yay

--- src/Components/Card/Cards.jsx
+++ src/Components/Card/Cards.jsx
@@ -1,12 +1,25 @@
 import React from "react";
-// import Buttons from "../Components/Button/Buttons";
+import { Card } from "react-bootstrap";
+import Buttons from "../Button/Buttons";
 import styling from "./Card.module.css";
 
 function Cards(props) {
   return (
     <>
       <div className={styling.wrapper}>
-        <div className={styling.container}></div>
+        <div className={styling.container}>
+          <Card style={{ width: "18rem" }}>
+            <Card.Img variant="top" src="../assets/martiniGlass.svg" />
+            <Card.Body>
+              <Card.Title>Drink Title</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </Card.Text>
+              <Buttons onClick={props.handleBtnClick}>Click Me</Buttons>
+            </Card.Body>
+          </Card>
+        </div>
       </div>
     </>
   );
